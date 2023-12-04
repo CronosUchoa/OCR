c52db75ce3006dd1550f3495c7204e8b1679060e
Cronos Uchoa <gabuhenrique1994@gmail.com>
2023-12-03 23:36:48 -0300

    Menu v.2 - Funcionando primeira opção é pra imagem simples

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import cv2
 import pytesseract as pt
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import numpy as np
 
-pt.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
+#2pt.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
 
 #função
 def remove_acentos(text):
@@ -34,20 +34,16 @@ def noise(image, prob):
   return output
 
 
-##
+## Menu0
 
-def Image1():
-  img = cv2.imread('Test.jpg')
-  # img_ruido = cv2.imread('comRuido.jpg')
-  # img_original = cv2.imread('Koala.jpg') #cv2.IMREAD_GRAYSCALE
-  img_clara_normalizacao = cv2.normalize(img, None, 5, 200, cv2.NORM_MINMAX)
-  img_escura_normalizacao = cv2.normalize(img, None, 1, 30, cv2.NORM_MINMAX)
-
-  img_ruido_pimenta = noise(img, 0.1)
-
+def ImagemSimplesCapturaDeCaracteres():
+  
+  pt.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
+ 
+  img = cv2.imread('./Imagens/Test.jpg')
 
   #@img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  #print(pt.pytesseract.image_to_string(img,lang='por'))
+  print(pt.pytesseract.image_to_string(img,lang='por'))
   boxes = pt.pytesseract.image_to_boxes(img)
   imgH,imgW, _ = img.shape
 
@@ -56,28 +52,10 @@ def Image1():
       letra,posicaoX,posicaoY,larguraW,alturaH =  remove_acentos(b[0]),int(b[1]),int(b[2]),int(b[3]),int(b[4])
     # cv2.rectangle(img, (posicaoX,imgH - posicaoY),(larguraW,imgH-alturaH),(0,0,255),2)
       cv2.rectangle(img, (posicaoX,imgH - posicaoY),(larguraW, imgH - alturaH),(0,0,255),2)
-      cv2.putText(img,letra,(posicaoX,imgH-posicaoY+25),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(0,0,255),2)
+    # cv2.putText(img,letra,(posicaoX,imgH-posicaoY+25),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(0,0,255),2)
 
-
-  #Tirando Ruido
-  median = cv2.medianBlur(img_ruido_pimenta,5)
-
-  #Mostrando Normalização
-  cv2.imshow('Imagem_clara_norma', img_clara_normalizacao)
-  cv2.imshow('Imagem_escura_norma', img_escura_normalizacao)
-
-
-  #Mostrando ruido e sem ruido
-  plt.subplot(121),plt.imshow(img_ruido_pimenta),plt.title('Com ruido')
-  plt.xticks([]),plt.yticks([])
-  plt.subplot(122),plt.imshow(median),plt.title('Sem ruido')
-  plt.xticks([]), plt.yticks([])
-  plt.show()
-
-  #Mostrando OCR
+  #Mostrando OCR1
   cv2.imshow('Imagem', img)
-
-
   cv2.waitKey(0)
   cv2.destroyAllWindows()
 
@@ -93,7 +71,7 @@ def default():
 
 def switch_case(option):
     switch_dict = {
-        1: Image1,
+        1: ImagemSimplesCapturaDeCaracteres,
         2: Image2,
         3: Image3
     }
@@ -102,7 +80,7 @@ def switch_case(option):
 # Loop principal
 while True:
     try:
-        opcao_escolhida = int(input("Digite a opção (0 para sair): "))
+        opcao_escolhida = int(input("Digite a opção 1,2,3 e (0 para sair): "))
         
         if opcao_escolhida == 0:
             print("Saindo do programa. Até logo!")
@@ -119,6 +97,27 @@ while True:
 
 
 
+#Tecnicas que talvez seja utilizados
+  # img_ruido = cv2.imread('comRuido.jpg')
+  # img_original = cv2.imread('Koala.jpg') #cv2.IMREAD_GRAYSCALE
+  # img_clara_normalizacao = cv2.normalize(img, None, 5, 200, cv2.NORM_MINMAX)
+  # img_escura_normalizacao = cv2.normalize(img, None, 1, 30, cv2.NORM_MINMAX)
 
+  # img_ruido_pimenta = noise(img, 0.1)
+
+ #Tirando Ruido
+ # median = cv2.medianBlur(img_ruido_pimenta,5)
+
+  #Mostrando Normalização
+  # cv2.imshow('Imagem_clara_norma', img_clara_normalizacao)
+  # cv2.imshow('Imagem_escura_norma', img_escura_normalizacao)
+
+
+  # #Mostrando ruido e sem ruido
+  # plt.subplot(121),plt.imshow(img_ruido_pimenta),plt.title('Com ruido')
+  # plt.xticks([]),plt.yticks([])
+  # plt.subplot(122),plt.imshow(median),plt.title('Sem ruido')
+  # plt.xticks([]), plt.yticks([])
+  # plt.show()
 
 
